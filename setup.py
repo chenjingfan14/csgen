ec101a5fa5b8110b9dcf05cd36b3e49ee715179e
Reece Otto <reece.otto@outlook.com>
2022-03-10 07:04:03 +1000

    Added setup and README files.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+# Setting up
+setup(name="csgen", 
+      version="0.1.0",
+      author="Reece Otto",
+      author_email="reece.otto@outlook",
+      description="A tool for creating compression system geometry for hypersonic vehicles.",
+      packages=find_packages(),
+)
